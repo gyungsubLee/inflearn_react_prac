9ed5782418a759c3ec711e9f54e393d0dd06f58d
gyoungsubLee <dltjq2323@gmail.com>
2025-01-19 16:43:30 +0900

    project 1: memo - useCallback을 사용한 최적화
    • useCallback 기본 사용
    • useState setter의 state 자동 매핑을 활용한 useCallback 내부의 state 의존성 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import MemoContainer from "./components/MemoContainer";
 import SideBar from "./components/SideBar";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { setItem, getItem } from "./lib/storage";
 import debounce from "lodash.debounce";
-
-const time = new Date().getTime();
 
 const debounceSetItem = debounce(setItem, 5000);
 
@@ -13,45 +11,65 @@ function App() {
   const [memos, setMemos] = useState(getItem("memos") || []);
   const [memoContainerIndex, setMemoContainerIndex] = useState(0);
 
-  const setMemo = (newMemo) => {
-    const newMemos = [...memos];
-    newMemos[memoContainerIndex] = newMemo;
+  const setMemo = useCallback(
+    (newMemo) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
 
-    setMemos(newMemos);
-    debounceSetItem("memos", newMemos);
-  };
+        newMemos[memoContainerIndex] = newMemo;
 
-  const addMemo = () => {
-    const now = new Date().getTime();
+        debounceSetItem("memos", newMemos);
 
-    const newMemos = [
-      ...memos,
-      {
-        title: "Untitled",
-        content: "",
-        createdAt: now,
-        updatedAt: now,
-      },
-    ];
+        return newMemos;
+      });
+    },
+    [memoContainerIndex]
+  );
 
-    setMemos(newMemos);
+  const addMemo = useCallback(() => {
+    setMemos((memos) => {
+      const now = new Date().getTime();
+
+      const newMemos = [
+        ...memos,
+        {
+          title: "Untitled",
+          content: "",
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+
+      debounceSetItem("memos", newMemos);
+
+      return newMemos;
+    });
+
+    // index Setter 함수에 memos state를 사용하고 있어, 의존성을 제거할 수 없다.
+    // 다른 state(memoContainerIndex)의 setter이기 때문에, memos state를 명시적으로 넣어줘야 한다.
     setMemoContainerIndex(memos.length);
-    debounceSetItem("memos", newMemos);
-  };
+  }, [memos]);
 
-  const deleteMemo = (index) => {
-    const newMemos = [...memos];
+  const deleteMemo = useCallback(
+    (index) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
 
-    newMemos.splice(index, 1);
+        newMemos.splice(index, 1);
 
-    setMemos(newMemos);
+        if (index === memoContainerIndex) {
+          setMemoContainerIndex(0);
+        } else if (index < memoContainerIndex) {
+          setMemoContainerIndex(memoContainerIndex - 1);
+        }
 
-    if (index === memoContainerIndex) {
-      setMemoContainerIndex(0);
-    }
+        debounceSetItem("memos", newMemos);
 
-    debounceSetItem("memos", newMemos);
-  };
+        return newMemos;
+      });
+    },
+    [memoContainerIndex]
+  );
 
   return (
     <div className="App">
